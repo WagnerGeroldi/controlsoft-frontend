d5127ffcc3810220be4907e9dbadf93f84ed8c6b
WagnerGeroldi <wagnergeroldi@gmail.com>
2022-08-10 16:33:32 -0300

    ajustes

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -11,11 +11,14 @@ const api = axios.create({
   },
 });
 
+api.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+api.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+api.defaults.headers.put['Access-Control-Allow-Origin'] = '*';
+api.defaults.headers.delete['Access-Control-Allow-Origin'] = '*';
 const user = getUserLocalStorage();
 
 if (user) {
   api.defaults.headers.common["x-access-token"] = user.token;
-  api.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 }
 
 export { api };
