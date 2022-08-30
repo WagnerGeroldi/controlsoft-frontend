d8554fd37463aaa8017ed230cd52b7815fadc8cd
WagnerGeroldi <wagnergeroldi@gmail.com>
2022-08-30 16:04:35 -0300

    add images

--- src/pages/Initial.tsx
+++ src/pages/Initial.tsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 
 /*imports MUI */
-
+import "./Initial.css";
 import Button from "@mui/material/Button";
 import { Head } from "./partials/Head";
+import imageLogin from "../assets/images/organize_resume.svg";
+import imageLoginRight from "../assets/images/login.svg";
 
 import { getUserLocalStorage } from "../state/SaveLocalStorage";
-import { HeaderDefault } from "./partials/HeaderDefault";
 import React from "react";
 
 export function Initial() {
@@ -15,44 +16,68 @@ export function Initial() {
   return (
     <>
       <Head title="Rede Unisoft - Controle de Estoque" />
-      <HeaderDefault />
-      <div className="card bg-light mt-5 p-5 w-100">
-        <div className="d-flex justify-content-center ">
-          {!userStorage ? (
-            <div className="d-flex gap-3 flex-column justify-content-center mx-auto">
-              <Link to="/login">
-                <Button
-                  className="mb-5 btn-default"
-                  variant="contained"
-                  color="primary"
-                >
-                  Clique para fazer login
-                </Button>
-              </Link>
-              <Link to="/register">
-                <Button
-                  className="btn-default"
-                  variant="contained"
-                  color="primary"
-                >
-                  Crie sua conta e começe agora
-                </Button>
-              </Link>
+      <div className="content-container">
+        <div className="left-board bg-light ">
+          <div className="mt-4 d-flex flex-column justify-content-center mx-auto">
+            <div className="mt-4 d-flex flex-column justify-content-center mx-auto">
+              <h2>Área de Login</h2>
+              <h3>Controle de Estoque Unisoft</h3>
             </div>
-          ) : (
-            <div className="d-flex gap-3 flex-column justify-content-center mx-auto">
-              <Link to={`/dashboard/${userStorage.id}`}>
-                <Button variant="contained" color="primary" type="submit">
-                  {`Continuar como ${userStorage.name}`}
-                </Button>
-              </Link>
-              <Link to="/login">
-                <Button color="primary">
-                  {`Não é ${userStorage.name} cique aqui e faça login`}
-                </Button>
-              </Link>
+          </div>
+          <div className="d-flex justify-content-center mt-4">
+            <img src={imageLogin} alt="" width="80%" />
+          </div>
+        </div>
+        <div className="right-board bg-info">
+          <div className="mt-4 d-flex flex-column justify-content-center">
+            <div className=" d-flex justify-content-center mb-5 ">
+              <img
+                src={imageLoginRight}
+                alt=""
+                width="50%"
+                className="image-info"
+              />
             </div>
-          )}
+            {!userStorage ? (
+              <div className="d-flex gap-3 flex-column justify-content-center mx-auto align-items-center">
+                <div className="d-flex justify-content-center">
+                  <Link to="/login">
+                    <Button
+                      className="mb-5 btn-default"
+                      variant="contained"
+                      color="primary"
+                    >
+                      Clique para fazer login
+                    </Button>
+                  </Link>
+                </div>
+                <Link to="/register">
+                  <Button
+                    className="btn-default"
+                    variant="contained"
+                    color="primary"
+                  >
+                    Crie sua conta e começe agora
+                  </Button>
+                </Link>
+              </div>
+            ) : (
+              <div className="d-flex gap-3 flex-column justify-content-center mx-auto">
+                <div className="d-flex justify-content-center">
+                  <Link to={`/dashboard/${userStorage.id}`}>
+                    <Button variant="contained" color="primary" type="submit">
+                      {`Continuar como ${userStorage.name}`}
+                    </Button>
+                  </Link>
+                </div>
+                <Link to="/login">
+                  <Button color="primary">
+                    {`Não é ${userStorage.name} cique aqui e faça login`}
+                  </Button>
+                </Link>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </>
